ef086030f3315e987ee4a03ee9fce4b0c97ef600
mattiamaffo <126592486+mattiamaffo@users.noreply.github.com>
2024-01-09 12:13:29 +0100

    Update T5_encoder.py

--- T5_encoder.py
+++ T5_encoder.py
@@ -1,30 +1,36 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
 
-def c_tensor():
-    tokenizer = T5Tokenizer.from_pretrained("t5-base")
-    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+text1 = "translate English to German: The house is wonderful."
+text2 = "translate English to German: The house is wonderful."
 
-    text = "translate English to German: The house is wonderful."
+input_sequences = [text1, text2]
 
-    # ENCODER PART WITH T5-MODEL
-    input_ids = tokenizer(text, return_tensors="pt").input_ids
+# Since we want the token's dimension to be 4, use a linear function that reduces the dimension.
+linear_layer = torch.nn.Linear(768, 4)
+
+# Tensors list
+conditioning_tensors = []
+
+# Through the input_sequences
+for input_sequence in input_sequences:
+    # ENCODER PART WITH T5-MODEL for each sentences
+    input_ids = tokenizer(input_sequence, return_tensors="pt").input_ids
     with torch.no_grad():
         encoder_output = model.encoder(input_ids).last_hidden_state
-
-    # Since we want the token's dimension to be 4, I use a linear function that reduce the dimension.
-    linear_layer = torch.nn.Linear(768, 4)
 
     # Generate the conditioning tensor, having as dimension T_C * D
     # where T_C = length of the sequence and D = embedding size.
     conditioning_tensor = linear_layer(encoder_output)
 
-    print("C dimension: ", conditioning_tensor.shape)
-    # output: [1, 11, 4] -> 11 = T_C = the length of the input's text
-    # ==> The sequence consists of 11 tokens or fragments of text.
-    # -> 4 = D = embedding size.
-    # ==> Each token in the sequence is represented by a 4-dimensional vector.
+    # add the conditioning tensor to the list
+    conditioning_tensors.append(conditioning_tensor)
 
-    # Corretto salvo?
-    return conditioning_tensor
+# Print the shapes and the tensors
+for i, tensor in enumerate(conditioning_tensors):
+    print(f"Dimensione di C per la frase {i + 1}: {tensor.shape}")
+    print(f"Valori di C per la frase {i + 1}: {tensor}")
+
